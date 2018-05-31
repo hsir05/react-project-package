a93d83b5ca69e7d16d1e6b2713b833daac98adc1
hdq <haqsir@163.com>
2018-05-31 17:57:37 +0800

    添加骨架屏，并测试

--- src/pages/me/me.jsx
+++ src/pages/me/me.jsx
@@ -2,10 +2,20 @@ import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 import { Button, WhiteSpace,WingBlank } from 'antd-mobile';
 import ReactPlaceholder from 'react-placeholder'
+import {TextBlock, MediaBlock, TextRow, RectShape, RoundShape} from 'react-placeholder/lib/placeholders';
 import 'react-placeholder/lib/reactPlaceholder.css';
 
 import './me.scss'
 
+const awesomePlaceholder = (
+  <div className='my-awesome-placeholder'>
+    <RectShape color='skyblue' style={{width: 30, height: 40}}/>
+    <TextBlock rows={3} color='orange'/>
+    <MediaBlock rows={3} color='green'/>
+    <TextRow rows={3}color='skyblue'/>
+    <RoundShape color='red' style={{width: 30, height: 30}}/>
+  </div>
+);
 
 export default class Me extends Component {
   constructor (props) {
@@ -28,10 +38,10 @@ export default class Me extends Component {
     )
     return (
       <div className='me'>
-        <ReactPlaceholder type='media' children={chil} rows={3} ready={this.state.ready}>
-
+        <ReactPlaceholder children={chil} ready={this.state.ready} customPlaceholder={awesomePlaceholder}>
+        {/* <ReactPlaceholder type='media' children={chil} rows={2} ready={this.state.ready}> */}
+          <Button type="warning">warning disabled</Button><WhiteSpace />
         </ReactPlaceholder>
-
       </div>
     )
   }
